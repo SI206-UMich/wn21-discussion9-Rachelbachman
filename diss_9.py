054c6430113938c9f4c1df18e4013a54bd5ba5c2
Rachel Bachman <rachelbachman@Rachels-MacBook-Air-2.local>
2021-03-24 11:27:44 -0400

    i am done with dicussion

--- diss_9.py
+++ diss_9.py
@@ -6,57 +6,78 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+	tags=soup.find_all('a', class_='ent-name')
+	link=tags[3].get('href')
+	return 'https://pokemondb.net'+link
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
-    url = 'https://pokemondb.net/pokedex/'+pokemon
-    #add code here
+	url = 'https://pokemondb.net/pokedex/'+pokemon
+	r=requests.get(url)
+	soup=BeautifulSoup(r.text, 'html.parser')
+	l=[]
+	tables= soup.find_all('table', class_='data-table')
+	table=tables[2]
+	body=table.find('tbody')
+	trs=body.find_all('tr')
+	for tr in trs:
+		tag=tr.find('a', class_='ent-name')
+		name=tag.text.strip()
+		l.append(name)
+	return l
+	#add code here
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
-    # initialize an empty list
-    
+	l=[]
+	ex=r'@\d{1,2} ?(?:am|pm)'
+	for sentence in sentences:
+		words=re.findall(ex, sentence)
+		for word in words:
+			l.append(word[1:])
+	return l
+	# initialize an empty list
+	
 
-    # define the regular expression
-    
+	# define the regular expression
+	
 
-    # loop through each sentence or phrase in sentences
-    
+	# loop through each sentence or phrase in sentences
+	
 
-    # find all the words that match the regular expression in each sentence
-       
+	# find all the words that match the regular expression in each sentence
+	   
 
-    # loop through the found words and add the words to your empty list
+	# loop through the found words and add the words to your empty list
 
 
-    #return the list of the last letter of all words that begin or end with a capital letter
+	#return the list of the last letter of all words that begin or end with a capital letter
 
 
 
 def main():
-    url = 'https://pokemondb.net/pokedex/national'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    getCharmanderLink(soup)
-    getEggMoves('scizor')
+	url = 'https://pokemondb.net/pokedex/national'
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	getCharmanderLink(soup)
+	getEggMoves('scizor')
 
 class TestAllMethods(unittest.TestCase):
-    def setUp(self):
-        self.soup = BeautifulSoup(requests.get('https://pokemondb.net/pokedex/national').text, 'html.parser')
+	def setUp(self):
+		self.soup = BeautifulSoup(requests.get('https://pokemondb.net/pokedex/national').text, 'html.parser')
 
-    def test_link_Charmander(self):
-        self.assertEqual(getCharmanderLink(self.soup), 'https://pokemondb.net/pokedex/charmander')
+	def test_link_Charmander(self):
+		self.assertEqual(getCharmanderLink(self.soup), 'https://pokemondb.net/pokedex/charmander')
 
-    def test_egg_moves(self):
-        self.assertEqual(getEggMoves('scizor'), ['Counter', 'Defog', 'Feint', 'Night Slash', 'Quick Guard'])
+	def test_egg_moves(self):
+		self.assertEqual(getEggMoves('scizor'), ['Counter', 'Defog', 'Feint', 'Night Slash', 'Quick Guard'])
 
-    def test_findLetters(self):
-        self.assertEqual(findLetters(['Come eat lunch at 12','there"s a party @2pm', 'practice @7am','nothing']), ['2pm', '7am'])
-        self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3 pm will be better']), ['12pm', '3 pm'])
+	def test_findLetters(self):
+		self.assertEqual(findLetters(['Come eat lunch at 12','there"s a party @2pm', 'practice @7am','nothing']), ['2pm', '7am'])
+		self.assertEqual(findLetters(['There is show @12pm if you want to join','I will be there @ 2pm', 'come at @3 pm will be better']), ['12pm', '3 pm'])
 
 if __name__ == "__main__":
-    main()
-    unittest.main(verbosity = 2)
+	main()
+	unittest.main(verbosity = 2)
